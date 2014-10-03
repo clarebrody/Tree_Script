6f7c44121d5cdf0ca09f47e0934558038a911341
clarebrody <clarebrody@gmail.com>
2014-10-04 11:55:21 +1300

    Simple os.walk example function.  Will modify later for pretty printing.

--- Tree.py
+++ Tree.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 
 
 def command_line_args():
-    parser = argparse.ArgumentParser(description='Short sample app')
-
-    parser.add_argument('directories', action='store', nargs='+', type=str)
+    parser = argparse.ArgumentParser(description='Directory tree script')
+    parser.add_argument('--extensions', '-e', action='store', nargs='*')
+    parser.add_argument('directories', action='store', nargs='+')
 
     return parser.parse_args()
 
 
+def walker(args):
+    for directory in args.directories:
+        for parent, sub_dirs, files in os.walk(directory):
+            print parent, sub_dirs, files
+
 def main():
     args = command_line_args()
-    print args
+    walker(args)
 
 if __name__ == '__main__':
     main()
